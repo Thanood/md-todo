16c4c3f0e60a91af0b02000fb24d01b7d2a15e00
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-06 21:17:52 +0100

    feat(app): add todo features

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+class Todo {
+  done: boolean;
+  title: string;
+}
+
 export class App {
+  addTodoModal: any;
+  keepModalOpen = false;
   message = 'Hello World!';
+  newTodo = {
+    title: ''
+  }
+  todos: Todo[] = [];
+
+  addTodo() {
+    this.addTodoModal.open();
+  }
+
+  saveTodo() {
+    this.todos.push({ done: false, title: this.newTodo.title });
+    this.newTodo.title = '';
+    if (!this.keepModalOpen) {
+      this.addTodoModal.close();
+    }
+  }
 }
--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,5 +1,8 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
-  //config.globalResources([]);
+  config.globalResources([
+    './attributes/md-auto-focus',
+    './elements/nav-bar'
+  ]);
 }
