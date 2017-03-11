473ea469547a165eaa0dfc6380ae06f6fe351085
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-11 18:42:07 +0100

    refactor(i18n): add route names

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+import { autoinject } from "aurelia-framework";
 import { RouterConfiguration, Router } from "aurelia-router";
+import {I18N} from 'aurelia-i18n';
 
+@autoinject()
 export class App {
   router: Router;
+
+  constructor(private i18n: I18N) { }
   
   configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
+    config.title = 'Todos';
     config.map([
       { route: '', redirect: 'all' },
-      { route: 'all', name: 'all', moduleId: 'views/todo-list', title: 'all', nav: true }
+      { route: 'all', name: 'all', moduleId: 'views/todo-list', title: this.i18n.tr('nav.all'), nav: true }
     ]);
   }
 }
